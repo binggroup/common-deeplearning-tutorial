52c15131d60ac75fcefba1cea47c757588907246
binggroup <hooh_group@qq.com>
2018-11-09 11:31:00 +0800

    cnn实现手写体识别算法lenet-5

---
+++ basic_cnn/mnist_onehot.py
@@ -0,0 +1,421 @@
+#-*- coding: utf-8 -*-
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow.python.framework import ops
+
+import math
+
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+import ssl
+
+
+keep_prob = 0.95
+DROP_OUT= False
+
+# 解决ssl错误
+_create_unverified_https_context = ssl._create_unverified_context
+ssl._create_default_https_context = _create_unverified_https_context
+
+# 下载数据集  默认存储到C:\Users\hurenbing\.keras\datasets\fashion-mnist
+mnist = keras.datasets.mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+# 标准化
+train_images = train_images/255.
+test_images = test_images/255.
+
+train_images = train_images.reshape(60000,28,28,1)
+test_images=test_images.reshape(10000,28,28,1)
+
+train_labels = train_labels.reshape([60000,1])
+test_labels = test_labels.reshape([10000,1])
+
+X_train = train_images[0:10000,:,:,:]
+X_test = test_images[0:1000,:,:,:]
+
+Y_train = train_labels[0:10000,:]
+Y_test = test_labels[0:1000,:]
+
+
+def convert_to_one_hot(Y, C):    
+    Y = np.eye(C)[Y.reshape(-1)]
+    return Y
+ 
+Y_train = convert_to_one_hot(Y_train,10)
+Y_test = convert_to_one_hot(Y_test,10)
+
+# Y_train = train_labels[Y_train ,1]
+# Y_train = train_labels[Y_test ,1]
+
+
+
+
+
+# class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+#                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+#查看数据的形状
+# print(train_images.shape)    #(60000, 28, 28)
+# print(train_labels.shape)
+# print(train_labels.shape)
+
+print(X_train.shape)
+print(X_test.shape)
+print(Y_train.shape)
+print(Y_test.shape)
+
+# 输出图像查看
+# plt.figure()
+# plt.imshow(train_images[0])
+# plt.colorbar()
+# plt.grid(False)
+# plt.show()
+
+# plt.figure(figsize=(10,10))
+# for i in range(25):
+#     plt.subplot(5,5,i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_images[i], cmap=plt.cm.binary)
+#     plt.xlabel( train_labels[i])
+# #     plt.xlabel(class_names[train_labels[i]])
+# plt.show()
+
+
+
+
+
+
+
+
+# model = keras.Sequential([
+#     keras.layers.Flatten(input_shape=(28, 28)),
+#     keras.layers.Dense(128, activation=tf.nn.relu),
+#     keras.layers.Dense(10, activation=tf.nn.softmax)
+# ])
+# 
+# model.compile(optimizer=tf.train.AdamOptimizer(), 
+#               loss='sparse_categorical_crossentropy',
+#               metrics=['accuracy'])
+# 
+# model.fit(train_images, train_labels, epochs=5)
+# 
+# test_loss, test_acc = model.evaluate(test_images, test_labels)
+# 
+# print('Test accuracy:', test_acc)
+# 
+# predictions = model.predict(test_images)
+# 
+# print(np.argmax(predictions[0]))
+
+#这个需要特别高明白，输入参数的shape
+def create_placeholders(n_H0, n_W0, n_C0, n_y):
+    """
+    Creates the placeholders for the tensorflow session.
+    
+    Arguments:
+    n_H0 -- scalar, height of an input image
+    n_W0 -- scalar, width of an input image
+    n_C0 -- scalar, number of channels of the input
+    n_y -- scalar, number of classes
+        
+    Returns:
+    X -- placeholder for the data input, of shape [None, n_H0, n_W0, n_C0] and dtype "float"
+    Y -- placeholder for the input labels, of shape [None, n_y] and dtype "float"
+    """
+
+    ### START CODE HERE ### (≈2 lines)
+    X = tf.placeholder(tf.float32, shape=(None, n_H0, n_W0, n_C0))
+    Y = tf.placeholder(tf.float32,shape=(None,n_y))
+    ### END CODE HERE ###
+    
+    return X, Y
+
+
+
+
+def initialize_parameters():
+    """
+    Initializes weight parameters to build a neural network with tensorflow. The shapes are:
+                        W1 : [4, 4, 3, 8]
+                        W2 : [2, 2, 8, 16]
+    Returns:
+    parameters -- a dictionary of tensors containing W1, W2
+    """
+    
+    tf.set_random_seed(1)                              # so that your "random" numbers match ours
+        
+    ### START CODE HERE ### (approx. 2 lines of code)
+    W1 =tf.get_variable('W1',[5,5,1,6],   initializer=tf.contrib.layers.xavier_initializer(seed = 0))
+    W2 = tf.get_variable('W2',[5,5,6,16],  initializer=tf.contrib.layers.xavier_initializer(seed = 0))
+    ### END CODE HERE ###
+    parameters = {"W1": W1,
+                  "W2": W2}
+    
+    return parameters
+
+# 实现lenet-5算法
+def forward_propagation(X, parameters):
+    """
+    Implements the forward propagation for the model:
+    CONV2D -> RELU -> MAXPOOL -> CONV2D -> RELU -> MAXPOOL -> FLATTEN -> FULLYCONNECTED
+
+    Arguments:
+    X -- input dataset placeholder, of shape (input size, number of examples)
+    parameters -- python dictionary containing your parameters "W1", "W2"
+                  the shapes are given in initialize_parameters
+
+    Returns:
+    Z3 -- the output of the last LINEAR unit
+    """
+
+    # Retrieve the parameters from the dictionary "parameters" 
+    W1 = parameters['W1']
+    W2 = parameters['W2']
+    
+    
+    # LENET-5 第一层卷积：  filter:5*5  s:1 pad:0   通道：6    CONV2D: stride of 1, padding 'SAME'
+    Z1 = tf.nn.conv2d(X,W1, strides = [1,1,1,1], padding = 'SAME')
+    # RELU
+    A1 = tf.nn.relu(Z1)
+    
+    ############drop out
+#     if DROP_OUT:
+#         A1 = tf.nn.dropout(A1, keep_prob)
+    
+    # 第一次池化：avg pool: window 2x2, sride 2, padding 'SAME'
+    P1 = tf.nn.avg_pool(A1,ksize=[1,2,2,1], strides = [1,2,2,1], padding = 'SAME')
+    # MAXPOOL
+#     P1 = tf.nn.max_pool(A1, ksize = [1,8,8,1], strides = [1,8,8,1], padding = 'SAME')
+    
+    
+    #第二层：    
+    # CONV2D: filters W2, stride 1, padding 'SAME'  通道数：16
+    Z2 = tf.nn.conv2d(P1, W2, strides = [1,1,1,1], padding = 'SAME')
+    # RELU
+    A2 = tf.nn.relu(Z2)
+#     if DROP_OUT:
+#         A2 = tf.nn.dropout(A2, keep_prob)
+    
+    # MAXPOOL: window 2x2, stride 2, padding 'SAME'
+    P2 = tf.nn.max_pool(A2, ksize = [1,2,2,1], strides = [1,2,2,1], padding = 'SAME')
+    
+    
+    # FLATTEN
+    P2 = tf.contrib.layers.flatten(P2)    
+    
+    # FULLY-CONNECTED without non-linear activation function (not not call softmax).
+    # 6 neurons in output layer. Hint: one of the arguments should be "activation_fn=None"
+    Z3 = tf.contrib.layers.fully_connected(P2, num_outputs = 120, activation_fn=tf.nn.relu)
+    ### END CODE HERE ###
+    if DROP_OUT:
+        Z3 = tf.nn.dropout(Z3, keep_prob)
+    
+    Z4 = tf.contrib.layers.fully_connected(Z3, num_outputs = 84, activation_fn=tf.nn.relu)
+    if DROP_OUT:
+        Z3 = tf.nn.dropout(Z4, keep_prob)
+    
+    #softmax    
+    Z5 = tf.contrib.layers.fully_connected(Z4, num_outputs = 10, activation_fn=None)
+
+
+    return Z5
+
+
+def compute_cost(Z5, Y):
+    """
+    Computes the cost
+    
+    Arguments:
+    Z3 -- output of forward propagation (output of the last LINEAR unit), of shape (6, number of examples)
+    Y -- "true" labels vector placeholder, same shape as Z3
+    
+    Returns:
+    cost - Tensor of the cost function
+    """
+    
+    ### START CODE HERE ### (1 line of code)
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=Z5, labels=Y))
+    ### END CODE HERE ###
+    
+    return cost
+
+
+
+
+def random_mini_batches(X, Y, mini_batch_size = 64, seed = 0):
+    """
+    Creates a list of random minibatches from (X, Y)
+    
+    Arguments:
+    X -- input data, of shape (input size, number of examples) (m, Hi, Wi, Ci)
+    Y -- true "label" vector (containing 0 if cat, 1 if non-cat), of shape (1, number of examples) (m, n_y)
+    mini_batch_size - size of the mini-batches, integer
+    seed -- this is only for the purpose of grading, so that you're "random minibatches are the same as ours.
+    
+    Returns:
+    mini_batches -- list of synchronous (mini_batch_X, mini_batch_Y)
+    """
+    
+    m = X.shape[0]                  # number of training examples
+    mini_batches = []
+    np.random.seed(seed)
+    
+    # Step 1: Shuffle (X, Y)
+    permutation = list(np.random.permutation(m))
+    shuffled_X = X[permutation,:,:,:]
+    shuffled_Y = Y[permutation,:]
+
+    # Step 2: Partition (shuffled_X, shuffled_Y). Minus the end case.
+    num_complete_minibatches = math.floor(m/mini_batch_size) # number of mini batches of size mini_batch_size in your partitionning
+    for k in range(0, num_complete_minibatches):
+        mini_batch_X = shuffled_X[k * mini_batch_size : k * mini_batch_size + mini_batch_size,:,:,:]
+        mini_batch_Y = shuffled_Y[k * mini_batch_size : k * mini_batch_size + mini_batch_size,:]
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+    
+    # Handling the end case (last mini-batch < mini_batch_size)
+    if m % mini_batch_size != 0:
+        mini_batch_X = shuffled_X[num_complete_minibatches * mini_batch_size : m,:,:,:]
+        mini_batch_Y = shuffled_Y[num_complete_minibatches * mini_batch_size : m,:]
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+    
+    return mini_batches
+
+def model(X_train, Y_train, X_test, Y_test, learning_rate = 0.009,
+          num_epochs = 100, minibatch_size = 64, print_cost = True):
+    """
+    Implements a three-layer ConvNet in Tensorflow:
+    CONV2D -> RELU -> MAXPOOL -> CONV2D -> RELU -> MAXPOOL -> FLATTEN -> FULLYCONNECTED
+    
+    Arguments:
+    X_train -- training set, of shape (None, 64, 64, 3)
+    Y_train -- test set, of shape (None, n_y = 6)
+    X_test -- training set, of shape (None, 64, 64, 3)
+    Y_test -- test set, of shape (None, n_y = 6)
+    learning_rate -- learning rate of the optimization
+    num_epochs -- number of epochs of the optimization loop
+    minibatch_size -- size of a minibatch
+    print_cost -- True to print the cost every 100 epochs
+    
+    Returns:
+    train_accuracy -- real number, accuracy on the train set (X_train)
+    test_accuracy -- real number, testing accuracy on the test set (X_test)
+    parameters -- parameters learnt by the model. They can then be used to predict.
+    """
+    
+    ops.reset_default_graph()                         # to be able to rerun the model without overwriting tf variables
+    tf.set_random_seed(1)                             # to keep results consistent (tensorflow seed)
+    seed = 3                                          # to keep results consistent (numpy seed)
+    
+    
+    (m, n_H0, n_W0, n_C0) = X_train.shape
+                 
+    n_y = Y_train.shape[1]                            
+    costs = []                                        # To keep track of the cost
+    
+    # Create Placeholders of the correct shape
+    ### START CODE HERE ### (1 line)
+    X, Y = create_placeholders(n_H0, n_W0, n_C0, n_y)
+    ### END CODE HERE ###
+
+    # Initialize parameters
+    ### START CODE HERE ### (1 line)
+    parameters = initialize_parameters()
+    ### END CODE HERE ###
+    
+    # Forward propagation: Build the forward propagation in the tensorflow graph
+    ### START CODE HERE ### (1 line)
+    Z5 = forward_propagation(X, parameters)
+    ### END CODE HERE ###
+    
+    # Cost function: Add cost function to tensorflow graph
+    ### START CODE HERE ### (1 line)
+    cost = compute_cost(Z5, Y)
+    ### END CODE HERE ###
+    
+    # Backpropagation: Define the tensorflow optimizer. Use an AdamOptimizer that minimizes the cost.
+    ### START CODE HERE ### (1 line)
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+    ### END CODE HERE ###
+    
+    # Initialize all the variables globally
+    init = tf.global_variables_initializer()
+     
+    # Start the session to compute the tensorflow graph
+    #sess = tf.Session(config=tf.ConfigProto(device_count={'GPU': 0}))     
+    with tf.Session(  ) as sess:
+#         with tf.Session( config=tf.ConfigProto(device_count={'GPU': 0}) ) as sess:
+        
+        # Run the initialization
+        sess.run(init)
+        
+        # Do the training loop
+        for epoch in range(num_epochs):
+
+            minibatch_cost = 0.
+            num_minibatches = int(m / minibatch_size) # number of minibatches of size minibatch_size in the train set
+            seed = seed + 1
+            minibatches = random_mini_batches(X_train, Y_train, minibatch_size, seed)
+
+            for minibatch in minibatches: 
+
+                # Select a minibatch
+                (minibatch_X, minibatch_Y) = minibatch
+                # IMPORTANT: The line that runs the graph on a minibatch.
+                # Run the session to execute the optimizer and the cost, the feedict should contain a minibatch for (X,Y).
+                ### START CODE HERE ### (1 line)
+                _ , temp_cost = sess.run([optimizer, cost], feed_dict={X:minibatch_X, Y:minibatch_Y})
+                ### END CODE HERE ###
+                
+                minibatch_cost += temp_cost / num_minibatches
+                
+
+            # Print the cost every epoch
+            if print_cost == True and epoch % 5 == 0:
+                print ("Cost after epoch %i: %f" % (epoch, minibatch_cost))
+            if print_cost == True and epoch % 1 == 0:
+                costs.append(minibatch_cost)
+        
+        
+        # plot the cost
+        plt.plot(np.squeeze(costs))
+        plt.ylabel('cost')
+        plt.xlabel('iterations (per tens)')
+        plt.title("Learning rate =" + str(learning_rate))
+        plt.show()
+
+        # Calculate the correct predictions
+        
+        predict_op = tf.argmax(Z5, 1)
+        correct_prediction = tf.equal(predict_op, tf.argmax(Y, 1))
+                
+        # Calculate accuracy on the test set
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+        
+        print(accuracy)
+        train_accuracy = accuracy.eval({X: X_train, Y: Y_train})
+        test_accuracy = accuracy.eval({X: X_test, Y: Y_test})
+        print("Train Accuracy:", train_accuracy)
+        print("Test Accuracy:", test_accuracy)
+                
+        return train_accuracy, test_accuracy, parameters
+#         return  parameters
+    
+
+if __name__=="__main__":
+    print('begin... ')
+    _, _, parameters = model(X_train, Y_train, X_test, Y_test)
+
+
+    
